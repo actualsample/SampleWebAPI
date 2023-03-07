90ab5270653b1119990d30da57fc24fd9c6cab99
actualsample <erick@actual-training.com>
2023-03-07 09:39:11 -0800

    menambahkan point of interest

---
+++ SampleApp.BackendAPI/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleApp.BackendAPI.Models;
+using System.Collections;
+
+namespace SampleApp.BackendAPI.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city);
+        }
+    }
+}
