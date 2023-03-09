468aaf7d6efee362084c1cb8bc92908ae7bc8f71
actualsample <erick@actual-training.com>
2023-03-08 16:32:43 -0800

    menambahkan relasi many2many

--- SampleApp.BackendAPI/Migrations/20230309002826_AddMany2ManyRelationship_Battle_Samurai.Designer.cs
+++ SampleApp.BackendAPI/Migrations/20230309002826_AddMany2ManyRelationship_Battle_Samurai.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SampleApp.BackendAPI.Data;
 
@@ -10,9 +11,10 @@ using SampleApp.BackendAPI.Data;
 namespace SampleApp.BackendAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230309002826_AddMany2ManyRelationship_Battle_Samurai")]
+    partial class AddMany2ManyRelationship_Battle_Samurai
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SampleApp.BackendAPI/Models/Battle.cs
+++ SampleApp.BackendAPI/Models/Battle.cs
@@ -3,6 +3,7 @@
     public class Battle
     {
         public int BattleId { get; set; }
-        public string Name { get; set; } 
+        public string Name { get; set; }
+        public List<Samurai> Samurais { get; set; }
     }
 }
