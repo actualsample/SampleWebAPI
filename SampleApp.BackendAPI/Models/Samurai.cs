e242ea2d0ca7006bce2cdccbfac30a08648fe0b4
actualsample <erick@actual-training.com>
2023-03-08 16:55:31 -0800

    one to one relationship

--- SampleApp.BackendAPI/Models/Samurai.cs
+++ SampleApp.BackendAPI/Models/Samurai.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public List<Quote> Quotes { get; set; }
         public List<Battle> Battles { get; set; }
+        public Horse Horse { get; set; }
     }
 }
